5b58e1c59e8b74795d1b22658a5bf7a7da593a32
Santiago Tucci <stucci@dc.uba.ar>
2019-12-04 19:36:19 -0300

    elimino lo que no utilizamos

--- 25-TusLibros/4/substrings-finder-6000/substrings-finder-6000 zip/client.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# auto compile on file changed
-echo "Start auto-compile" &
-python ./scripts/autoreload.py python ./scripts/compiler.py &
-echo "Start browser auto-refresh" &
-# browser refresh on file changed
-python ./scripts/refreshbrowser.py
